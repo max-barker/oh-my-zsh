1e66e790b06eef696d0949e92b172e67a7faa180
Max <max@stannp.com>
2025-02-24 14:19:19 +0000

    added aliases

---
+++ custom/php.zsh
@@ -0,0 +1,10 @@
+alias phinx='./vendor/bin/phinx'
+alias seed='phinx seed:run'
+alias migrate='phinx migrate'
+
+alias pest='./vendor/bin/pest'
+alias p='./vendor/bin/phpunit --colors'
+alias pv='p --verbose'
+alias pf='p --filter'
+alias pg='p --group'
+alias pa=" docker exec webserver php bin/console"
