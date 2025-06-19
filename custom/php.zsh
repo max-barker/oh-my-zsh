1089bfae541ea34dbf777ee96ff182708905950b
Max <max@stannp.com>
2025-06-19 12:56:51 +0100

    added xedbug_config to php helpers

--- custom/php.zsh
+++ custom/php.zsh
@@ -1,3 +1,5 @@
+export XDEBUG_CONFIG="idekey=VSCODE"
+
 alias phinx='./vendor/bin/phinx'
 alias seed='phinx seed:run'
 alias migrate='phinx migrate'
@@ -7,4 +9,4 @@ alias p='./vendor/bin/phpunit --colors'
 alias pv='p --verbose'
 alias pf='p --filter'
 alias pg='p --group'
-alias pa=" docker exec webserver php bin/console"
+alias pa="docker exec webserver php bin/console"
