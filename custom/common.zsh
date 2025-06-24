34d281b98be10c4decbac9c7ebe4a3a145080d8d
Max Barker <max@the-barkers.com>
2025-06-24 21:50:38 +0100

    run ll after cd

--- custom/common.zsh
+++ custom/common.zsh
@@ -1,4 +1,8 @@
 alias ls='ls --color=auto'
 alias ll='ls -lah'
 
+# Register ll to run after each change of directory
+autoload -U add-zsh-hook
+add-zsh-hook -Uz chpwd (){ ll; }
+
 alias refresh='source ~/.zshrc'
